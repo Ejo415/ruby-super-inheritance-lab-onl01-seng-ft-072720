82e47e17f470024693dda3e909f522aa990b472b
Eugene O'Neil <ejo7220@gmail.com>
2020-08-12 14:27:58 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -4,10 +4,34 @@ class ChattyStudent < Student
     super
     puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
+<<<<<<< HEAD
   
   def raise_hand
     10.times do
       super
     end 
+=======
+
+  def raise_hand
+    10.times {super}
+  end
+end
+
+  end
+  
+  def raise_hand
+    super
+    puts   "-Pick me!
+        -Pick me!
+        -Pick me!
+        -Pick me!
+        -Pick me!
+        -Pick me!
+        -Pick me!
+        -Pick me!
+        -Pick me!
+        -Pick me!
+  "
+>>>>>>> ba583ade3f9c5f01dbc7b20c1d09bd2c8017ff4c
      end
 end
